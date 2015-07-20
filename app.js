77772a0bbf8697fa03c3687fae0586e5f852874f
Mario Cuba <mario@mariocuba.net>
2015-07-20 23:05:26 +0100

    Adjustments to app file so it works on deployment.

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
-// This is the main file of our chat app. It initializes a new 
+// This is the main file of our chat app. It initializes a new
 // express.js instance, requires the config and routes files
 // and listens on a port. Start the application by running
 // 'node app.js' in your terminal
 
 
 var express = require('express'),
-	app = express();
+        app = express();
 
 // This is needed if the app is run on heroku:
 
 var port = process.env.PORT || 8080;
 
-// Initialize a new socket.io object. It is bound to 
+// Initialize a new socket.io object. It is bound to
 // the express app, which allows them to coexist.
 
-var io = require('socket.io').listen(app.listen(port));
+// var io = require('socket.io').listen(app.listen(port), '46.101.34.63');
+var socketio = require('socket.io');
+var server = app.listen(port, '46.101.34.63');
+var io = require('socket.io').listen(server);
 
 // Require the configuration and the routes files, and pass
 // the app and io as arguments to the returned functions.
@@ -22,4 +25,4 @@ var io = require('socket.io').listen(app.listen(port));
 require('./config')(app, io);
 require('./routes')(app, io);
 
-console.log('Your application is running on http://localhost:' + port);
+console.log('Your application is running on http://46.101.34.63:' + port);
